ae7b7ea7559d5760067dc31b79c8bc0a7a8defcd
U Prashanth <uprashanth1994@outlook.com>
2022-02-11 02:02:52 +0530

    finished adding movie details page

--- src/pages/movie.tsx
+++ src/pages/movie.tsx
@@ -21,101 +21,57 @@ const Container = Styled.div`
     justify-content: center;
 `
 
-const MovieTitle = Styled.p`
-    font-size: 20px;
-    font-weight: 600;
-    color: #fff;
+const MovieDetailsSection = Styled.div`
+    max-width: 1200px;
+    width: 100%;
+    height: 100%;
+
+    margin: 0px auto;
+    margin-top: -120px;
+    padding: 30px;
+
+    background-color: #1e1e1e;
+    border-radius: 8px;
+
+    position: relative;
+    z-index: 10;
 `
 
-const OverviewText = Styled(MovieTitle)`
-    font-size: 14px;
+const Title = Styled.p`
+    color: #fff;
+    font-size: 1.2rem;
+    font-weight: 700;
+
+    @media only screen and (max-width: 800px)
+    {
+        font-size: 14px;
+    }
+`
+const Overview = Styled.p`
+    color: #e1e1e1;
+    font-size: .8rem;
+    font-weight: 400;
+    line-height: 30px;
+    margin-top: 16px;
+
+    @media only screen and (max-width: 800px)
+    {
+        margin-top: 8px;
+        font-size: 12px;
+        line-height: 18px;
+    }
 `
 
 const VideoWrapper = Styled.div`
     width: 100%;
     height: 60%;
+    margin: 24px 0px;
 
     @media only screen and (max-width: 1000px)
     {
         height: 40%;
     }
-
-    margin-bottom: 12px;
 `
-
-// interface IMovieProps extends GetServerSidePropsContext
-// {
-//     router: NextRouter
-//     data: string;
-// }
-
-// interface IState
-// {
-//     movieData?: IMovieData;
-// }
-
-// class Movie extends React.Component<IMovieProps, IState>
-// {
-//     constructor(props: IMovieProps)
-//     {
-//         super(props);
-
-//         this.state = {
-//             movieData: {}
-//         }
-//     }
-
-//     componentDidMount()
-//     {
-//         this.setMovieData(this.props.data);
-//     }
-
-//     componentDidUpdate(prevProps: Readonly<IMovieProps> & Readonly<{ children?: React.ReactNode; }>)
-//     {
-//         if(this.props.router.isReady !== prevProps.router.isReady || this.props !== prevProps)
-//             this.setMovieData(this.props.data);
-//     }
-
-//     setMovieData = (value: string) => {
-//         this.setState({ movieData: JSON.parse(value) as IMovieData });
-//     }
-
-//     shouldComponentUpdate(prevProps: Readonly<IMovieProps> & Readonly<{ children?: React.ReactNode; }>)
-//     {
-//         return !(this.props !== prevProps);
-//     }
-
-//     render()
-//     {
-//         return (
-//             <Page title={this.state.movieData?.title || ''}>
-//                 <Container>
-//                     <VideoWrapper>
-//                         <div className='embed-container'>
-//                             <iframe src='https://www.youtube.com/embed/ZYzbalQ6Lg8' frameBorder='0' allowFullScreen></iframe>
-//                         </div>
-//                     </VideoWrapper>
-
-//                     <MovieTitle>{this.state.movieData?.title || ''}</MovieTitle>
-//                     <MovieTitle>{this.state.movieData?.overview || ''}</MovieTitle>
-
-
-//                 </Container>
-//             </Page>
-//         )    
-//     }
-// }
-
-// export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-//     return {
-//         props: {
-//             data: JSON.stringify((await GetMovieDetailsService(ctx.query.id! as string)).data)
-//         }
-//     }
-// }
-
-// export default withRouter(Movie);
-
 
 interface IMovieProps
 {
@@ -127,6 +83,17 @@ const Movie: React.FunctionComponent<IMovieProps> = (props) => {
         <Page title={`Netflix | ${props.movieDetails.title}` || ''}>
             <Container>
                 <MovieBackdrop backdropURL={props.movieDetails.backdrop_path!}/>
+
+                <MovieDetailsSection>
+                    <Title>{props.movieDetails.title!}</Title>
+                    <Overview>{props.movieDetails.overview!}</Overview>
+
+                    <VideoWrapper>
+                        <div className='embed-container'>
+                            <iframe src='https://www.youtube.com/embed/ZYzbalQ6Lg8' frameBorder='0' allowFullScreen></iframe>
+                        </div>
+                    </VideoWrapper>
+                </MovieDetailsSection>
             </Container>
         </Page>
     )
